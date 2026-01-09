99e4ab0ba2c87802c0a6a85a1bd2e5b386218618
Olivier Adler <olivieradler@Ordinateur-portable-de-Olivier.local>
2026-01-09 19:09:42 +0100

    Add web interface for questionnaire

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Static files
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
